6120f4b720dc94897d484997a47461211133d9ae
adambernstahle <adam.bernstahle@gmail.com>
2017-09-22 08:46:32 +0200

    TESTING

--- InlamningsUpggift 2 Adam Bernstahle RIKTIG/Program.cs
+++ InlamningsUpggift 2 Adam Bernstahle RIKTIG/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
-namespace InlamningsUpggift_2_Adam_Bernstahle_RIKTIG
+namespace Exmpel1
 {
     class Program
     {
+        static void SkrivUtVeckodagar()
+        {
+            Console.WriteLine("Måndag");
+            Console.WriteLine("Tisdag");
+            Console.WriteLine("Onsdag");
+            Console.WriteLine("Torsdag");
+            Console.WriteLine("Fredag");
+            Console.WriteLine("Lördag");
+            Console.WriteLine("Söndag");
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            SkrivUtVeckodagar();
+
+            //Här görs något annat, t.ex inmatning eller beräkningar
+            //för exemplet är det inte instressant. Poängen är att
+            //vi vill upprepa samma utrskift som vi gjort tidigare 
+            //en gång till vid ett senare tillfälle.
+
+            SkrivUtVeckodagar();
         }
     }
 }
