02b75d3a6084dd871ce753a64425512c090c996b
adambernstahle <adam.bernstahle@gmail.com>
2017-09-22 08:40:46 +0200

    Add project files.

---
+++ InlamningsUpggift 2 Adam Bernstahle RIKTIG/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InlamningsUpggift_2_Adam_Bernstahle_RIKTIG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
